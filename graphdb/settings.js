f3aac1c691868da27992704642842d429d6d4163
Flo34q5 <FMDiehl@gmx.de>
2024-12-27 19:57:41 +0100

    Add files via upload

---
+++ graphdb/settings.js
@@ -0,0 +1,87 @@
+{
+  "properties" : {
+    "current.location" : "",
+    "security.free.access.authorities" : ""
+  },
+  "import.local" : {
+    "Homework9_Ontology;;homework9-ontology.ttl" : {
+      "name" : "homework9-ontology.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "default",
+      "replaceGraphs" : [ "default" ],
+      "baseURI" : "file:/uploaded/generated/homework9-ontology.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "453bb8e9-761d-48f4-b723-41d4481e3166",
+      "timestamp" : 1702626775070,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "homework9_provided_data;;homework9.nt" : {
+      "name" : "homework9.nt",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "default",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/homework9.nt",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "1638041a-9ee6-401a-a614-deaceddbc6a1",
+      "timestamp" : 1702626962129,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "homework9_ontology;;homework9-ontology.ttl" : {
+      "name" : "homework9-ontology.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "default",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/homework9-ontology.ttl",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "9a9b1f12-abd8-434c-b786-1ef42f3418d2",
+      "timestamp" : 1702626998529,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    }
+  },
+  "locations" : {
+    "" : {
+      "location" : "",
+      "authType" : "none",
+      "password" : null,
+      "username" : null,
+      "defaultRepository" : null
+    }
+  },
+  "import.server" : { }
+}
